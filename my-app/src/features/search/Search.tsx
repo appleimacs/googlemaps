863cfd56efebed1d4970d0be5b4504105e02fc4c
jon raymond melchizedek day <jonrmday@gmail.com>
2023-01-27 15:25:42 -0800

    removing extra getresults call

--- my-app/src/features/search/Search.tsx
+++ my-app/src/features/search/Search.tsx
@@ -1,20 +1,18 @@
 import { useAppSelector, useAppDispatch } from '../../app/hooks';
-import { getResults, selectCenter, selectMap, setCenter, setFilter } from '../../app/reducers/resultSlice';
+import { selectMap, setCenter, setFilter } from '../../app/reducers/resultSlice';
 import styles from './Search.module.css';
 
 const Search = () => {
   const dispatch = useAppDispatch();
-  const center = useAppSelector(selectCenter);
   const map = useAppSelector(selectMap);
 
   const handleSearch = (e: any) => {
       if(e.key === 'Enter' || e.keyCode === 13){
           const filter = e.target.value;
           const newCenter = {lng: map.getCenter().lng(), lat: map.getCenter().lat()}
-          
+
           dispatch(setCenter({center: newCenter}))
           dispatch(setFilter({filter: filter}))
-          dispatch(getResults({filter: filter, center:center}))
       }
   }
 
